a9d530244495346ab21c366cead40235aa9c515c
Cody Troop <codytroop@yahoo.com>
2019-02-01 16:38:23 -0600

    Boilerplates added so each component can be called although empty

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import ProjectPage from '../ProjectPage/ProjectPage.js';
+import Admin from '../Admin/Admin.js';
 
 class App extends Component {
   // Renders the entire app on the DOM
---
+++ src/components/ProjectTable/ProjectTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProjectTable extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default ProjectTable;
