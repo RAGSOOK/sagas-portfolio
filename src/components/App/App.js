ca15c56e054e3f12c06597263e6fb638dd984d09
Cody Troop <codytroop@yahoo.com>
2019-02-01 16:53:40 -0600

    Added Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ProjectPage from '../ProjectPage/ProjectPage.js';
 import Admin from '../Admin/Admin.js';
+import Header from '../Header/Header.js';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Header />
+
+        <Router>
+          <div>
+            <Route exact path="/" component={ProjectPage} />
+            <Route exact path="/admin" component={Admin} />
+          </div>
+        </Router>
       </div>
     );
   }
