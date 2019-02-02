3419366c3c73a33c4d3896f5982288640d615cee
Cody Troop <codytroop@yahoo.com>
2019-02-01 19:01:35 -0600

    server router files exist with GETS

--- server/server.js
+++ server/server.js
@@ -3,11 +3,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+//routes
+const projectRouter = require('./routes/projects.router.js');
+const tagsRouter = require('./routes/tags.router.js');
+
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//serve statics
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
+app.use('/projects', projectRouter);
+app.use('/tags', tagsRouter);
 
 
 /** ---------- START SERVER ---------- **/
